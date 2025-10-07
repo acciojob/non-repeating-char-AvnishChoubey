5c67296f5ea6be7d91af1bccc3eb7a4e6ce7c815
Avnish Choubey <118668098+AvnishChoubey@users.noreply.github.com>
2025-10-08 00:52:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let arr = {};
+	
+	for(let i=0;i<str.length;i++)
+	{
+		let char = str[i];
+		arr[char] = (arr[char] || 0) + 1;
+	}
+
+	for(let i=0;i<str.length;i++)
+	{
+		if(arr[str[i]] === 1)
+			return str[i];
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
